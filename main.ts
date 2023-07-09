b34b35e8a476c9b464e1993a842db9fdb2ee0de1
aedans <aedan.smith@outlook.com>
2023-07-09 13:23:09 -0700

    Initial implementation

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { syntaxTree } from "@codemirror/language";
+import { RangeSetBuilder } from "@codemirror/state";
+import {
+	Decoration,
+	DecorationSet,
+	EditorView,
+	PluginSpec,
+	PluginValue,
+	ViewPlugin,
+	ViewUpdate,
+	WidgetType,
+} from "@codemirror/view";
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { nanoid } from "nanoid";
+import moize from "moize";
 
 // Remember to rename these classes and interfaces!
 
@@ -7,8 +21,143 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: "default",
+};
+
+export const getScryfallCard = moize.promise(async (name: string) => {
+	const response = await fetch(
+		`https://api.scryfall.com/cards/named?exact=${encodeURIComponent(name)}`
+	);
+	if (response.ok) {
+		const json = await response.json();
+		return json.image_uris.normal;
+	} else {
+		return null;
+	}
+});
+
+export class CardWidget extends WidgetType {
+	constructor(public id: string, public name: string) {
+		super();
+	}
+
+	toDOM(view: EditorView): HTMLElement {
+		const img = document.createElement("img");
+		img.className = "scryfall_card";
+		img.width = 488 / 2;
+		img.id = this.id;
+		img.style.opacity = "0";
+		img.toggleVisibility(false);
+
+		const listener: {
+			current: (this: HTMLElement, e: MouseEvent) => void;
+		} = {
+			current: () => {},
+		};
+
+		getScryfallCard(this.name).then((src) => {
+			if (src == null) {
+				return;
+			}
+
+			img.src = src;
+
+			listener.current = (e) => {
+				if (!img) {
+					view.dom.removeEventListener("mousemove", listener.current);
+					return;
+				}
+
+				const rect = view.dom.getBoundingClientRect();
+				img.style.left = e.clientX - rect.left + 10 + "px";
+				img.style.top = e.clientY - rect.top + 40 + "px";
+
+				if (img.style.opacity != "100%") {
+					img.style.opacity = "100%";
+				}
+
+				const element = document.getElementById(`${this.id}-a`);
+				if (element) {
+					element.onmousedown = () => {
+						const urlName = encodeURIComponent(this.name);
+						const url = `https://scryfall.com/search?as=grid&order=released&q=%21%22${urlName}%22`;
+						// eslint-disable-next-line @typescript-eslint/no-var-requires
+						require("electron").shell.openExternal(url);
+					};
+				}
+			};
+
+			view.dom.addEventListener("mousemove", listener.current);
+		});
+
+		return img;
+	}
 }
+
+export class CardPlugin implements PluginValue {
+	decorations: DecorationSet;
+
+	constructor(view: EditorView) {
+		this.decorations = this.buildDecorations(view);
+	}
+
+	update(update: ViewUpdate) {
+		if (update.docChanged || update.viewportChanged) {
+			this.decorations = this.buildDecorations(update.view);
+		}
+	}
+
+	destroy() {}
+
+	buildDecorations(view: EditorView): DecorationSet {
+		const builder = new RangeSetBuilder<Decoration>();
+
+		for (const { from, to } of view.visibleRanges) {
+			syntaxTree(view.state).iterate({
+				from,
+				to,
+				enter(node) {
+					if (node.type.name == "hmd-barelink_link") {
+						const id = nanoid();
+						const name = view.state.doc
+							.slice(node.from, node.to)
+							.toString()
+							.toLowerCase();
+
+						builder.add(
+							node.from,
+							node.to,
+							Decoration.mark({
+								tagName: "a",
+								attributes: {
+									id: `${id}-a`,
+									onmouseover: `document.getElementById("${id}")?.toggleVisibility(true)`,
+									onmouseout: `document.getElementById("${id}")?.toggleVisibility(false)`,
+								},
+							})
+						);
+
+						builder.add(
+							node.to,
+							node.to,
+							Decoration.widget({
+								widget: new CardWidget(id, name),
+							})
+						);
+					}
+				},
+			});
+		}
+
+		return builder.finish();
+	}
+}
+
+const pluginSpec: PluginSpec<CardPlugin> = {
+	decorations: (value: CardPlugin) => value.decorations,
+};
+
+export const cardPlugin = ViewPlugin.fromClass(CardPlugin, pluginSpec);
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,94 +165,24 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.registerEditorExtension(cardPlugin);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -116,22 +195,24 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
